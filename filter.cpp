1fb5ba88da606480659c79a875c8347a9b46ee48
yujietaipei <noodles963@gmail.com>
2022-11-29 14:53:19 +0800

    filter2.0

--- filter.cpp
+++ filter.cpp
@@ -1,42 +1,81 @@
 #include "filter.h"
+//the image is not centered after transform, and hence, the filter is not centered, either
 
-cv::Mat* ideal_highpass_filter(int width, int height, int radius){
-    //the image is not shifted after fft, hence, the filter must be shifted
-    cv::Mat* filter = new cv::Mat(height,width,CV_8UC1,cv::Scalar(1));
-    int cx = width/2;
-    int cy = height/2;
-
+cv::Mat* build_filter(int height, int width, std::vector<int>&  params, float(*fun)(int , int ,std::vector<int>&) ){
+    cv::Mat* filter = new cv::Mat(height,width,CV_32FC1,cv::Scalar(0));
+    int cx = width >> 1;
+    int cy = height >> 1;
     for(int i = 0;i < height;i++){
         for(int j = 0;j < width;j++){
             int dist_x = (i + cx)%width - cx;
             int dist_y = (j + cy)%height - cy;
-
-            if(dist_x*dist_x + dist_y*dist_y < radius * radius){
-                //if the cooresponding point in filter is in the cicle
-                filter -> at<uchar>(i,j) = 0;
-            }
+            filter -> at<float>(i,j) = fun(dist_x,dist_y,params);
         }
     }
     return filter;
 }
 
-cv::Mat* ideal_lowpass_filter(int height,int width,int radius){
-    //the image is not shifted after fft, hence, the filter is shifted
-    cv::Mat* filter = new cv::Mat(height,width,CV_32FC1,cv::Scalar(0));
-    int cx = width/2;
-    int cy = height/2;
+float ideal_highpass(int disX, int disY, std::vector<int>& params){
+    int cutOff = params[0];
+    return (disX * disX + disY * disY < cutOff * cutOff ? 0 : 1);
+}
 
-    for(int i = 0;i < height;i++){
-        for(int j = 0;j < width;j++){
-            int dist_x = (i + cx)%width - cx;
-            int dist_y = (j + cy)%height - cy;
-            if(dist_x*dist_x + dist_y*dist_y < radius * radius){
-                //if the cooresponding point in filter is in the cicle
-                filter -> at<float>(i,j) = 1;
-            }
-        }
-    }
-    return filter;
+cv::Mat* ideal_highpass_filter(int height, int width, int cutOff){
+    std::vector <int> params{cutOff};
+    return build_filter(height, width, params, ideal_highpass);
+}
+
+float gaussian_highpass(int disX, int disY,std::vector<int>& params){
+    //disX and distY is the distance from center to specific coordination
+    int cutOff = params[0];
+    return 1 - exp(-static_cast<float>(disX*disX + disY*disY)/(2 * cutOff * cutOff));
+}
+
+cv::Mat* gaussian_highpass_filter(int height, int width, int cutOff){
+    std::vector<int> params {cutOff};
+    return build_filter(height, width, params, gaussian_highpass);
+}
+
+float butterworth_highpass(int disX, int disY, std::vector<int>& params){
+    int cutOff = params[0];
+    int pow_n = params[1];
+    return 1 / (1 + pow(static_cast<float>(cutOff * cutOff)/(disX * disX + disY * disY), pow_n));
+}
+
+cv::Mat* butterworth_highpass_filter(int height, int width, int cutOff, int pow_n){
+    std::vector<int> params {cutOff, pow_n};
+    return build_filter(height, width, params, butterworth_highpass);
+}
+
+float ideal_lowpass(int disX, int disY, std::vector<int>& params){
+    int cutOff = params[0];
+    return (disX * disX + disY * disY < cutOff * cutOff ? 1 : 0);
+}
+
+cv::Mat* ideal_lowpass_filter(int height,int width,int cutOff){
+    std::vector<int> params{cutOff};
+    return build_filter(height, width,params,ideal_lowpass);
+}
+
+float gaussin_lowpass(int disX, int disY, std::vector<int>& params){
+    int cutOff = params[0];
+    return exp(-static_cast<float>(disX * disX + disY * disY)/(2 * cutOff*cutOff));
+}
+
+cv::Mat* gaussian_lowpass_filter(int height, int width, int cutOff){
+    std::vector<int> params{cutOff};
+    return build_filter(height,width,params,gaussin_lowpass);
+}
+
+float butterworth_lowpass(int disX, int disY, std::vector<int>& params){
+    int cutOff = params[0];
+    int pow_n = params[1];
+    return 1 / (1 + pow(static_cast<float>(disX * disX + disY * disY)/(cutOff*cutOff), pow_n));
+}
+
+cv::Mat* butterworth_lowpass_filter(int height, int width, int cutOff, int pow_n){
+    std::vector<int> params {cutOff, pow_n};
+    return build_filter(height, width, params, butterworth_lowpass);
 }
 
 void image_process(cv::Mat& imag, const cv::Mat& filter){
--- filter.h
+++ filter.h
@@ -3,11 +3,36 @@
 
 #include <opencv2/opencv.hpp>
 
-//create ideal highpass filter with specified size
-cv::Mat* ideal_highpass_filter(int width, int height, int radius);
+//function to build the filter (width, height) is the size of the filter
+//params is the parameters for filter function (Gaussian, ideal etc.)
+//fun is funcion pointer of filter function
+cv::Mat* build_filter(int width, int height, std::vector<int>& params, float(*fun)(int, int, std::vector<int>&) );
 
-//create ideal lowpass filter with specified size
-cv::Mat* ideal_lowpass_filter(int height,int width,int radius);
+//XXX_highpass and XXX_lowpass are functions of filter
+//XXX_highpass_filter and XXX_lowpass_filter are filter generation functions (for users)
+float ideal_highpass(int disX, int disY, std::vector<int>& params);
+
+cv::Mat* ideal_highpass_filter(int width, int height, int cutOff);
+
+float gaussian_highpass(int disX, int disY, std::vector<int> & params);
+
+cv::Mat* gaussian_highpass_filter(int width, int height, int cutOff);
+
+float butterworth_highpass(int disX, int disY, std::vector<int>& params);
+
+cv::Mat* butterworth_highpass_filter(int height, int width, int cutOff, int pow_n);
+
+float ideal_lowpass(int disX, int disY, std::vector<int>& params);
+
+cv::Mat* ideal_lowpass_filter(int height,int width,int cutOff);
+
+float gaussin_lowpass(int disX, int disY, std::vector<int>& params);
+
+cv::Mat* gaussian_lowpass_filter(int height, int width, int cutOff);
+
+float butterworth_lowpass(int disX, int disY, std::vector<int>& params);
+
+cv::Mat* butterworth_lowpass_filter(int height, int width, int cutOff, int pow_n);
 
 //use filter to process the image in frequency domain
 void image_process(cv::Mat& imag, const cv::Mat& filter);
